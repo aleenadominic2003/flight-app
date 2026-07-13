813a7f70709454dc8d44255dfc817b09724df9e5
aleenadominic2003 <aleenadominic16@gmail.com>
2026-07-13 12:59:23 +0530

    Navigation done

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <BrowserRouter>
       
+        <Routes>
+
+          
+          <Route path='/' element={<AddFlight/>} />
+          <Route path='/view' element={<ViewAllFlights/>} />
+
+        </Routes>
+      
+      </BrowserRouter>
     </>
   )
 }
